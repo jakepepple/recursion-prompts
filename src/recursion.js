a7e5883e6d5f0547af499ad081613ea22b247618
Jake Pepple <jakepeppledrums@gmail.com>
2017-08-02 13:48:16 -0500

    Solve through #23

--- src/recursion.js
+++ src/recursion.js
@@ -430,6 +430,20 @@ var countValuesInObj = function(obj, value) {
 // 23. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, key, newKey) {
+	
+	for(var mykey in obj){
+		
+		if(mykey === key){
+			var newValue = obj[key];
+			obj[newKey] = newValue;
+			delete obj[key];
+			
+		}
+		if(obj[mykey] instanceof Object){
+			replaceKeysInObj(obj[mykey], key, newKey);
+		}
+	}
+	return obj;
 };
 
 // 24. Get the first n Fibonacci numbers.  In the Fibonacci Sequence, each subsequent
